8ca1c0cea648544567e3b4cf5f18a51a2e388c54
Daniel Garza <danielg272313@gmail.com>
2024-08-29 08:43:55 -0600

    complete basic chapter 1 assignment

--- bmi.js
+++ bmi.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 
-let weight = parseFloat(prompt('What is your weight in Kilograms?: '));
-let height = parseFloat(prompt('What is your height in meters?: '));
+let weight = parseFloat(prompt('Enter your weight in kilograms: '));
+let height = parseFloat(prompt('Enter your height in meters: '));
 
 let bmi = weight / (height * height)
 
@@ -9,6 +9,15 @@ function weightCategory(bmi){
     if (bmi < 18.5){
         return 'Underweight'
     }
+    else if (bmi < 25){
+        return 'Normal weight'
+    }
+    else if (bmi < 30){
+        return 'Overweight'
+    }
+    else {
+        return 'Obese'
+    }
 }
 
-console.log(weightCategory(5))
+console.log('Your BMI is ' + bmi.toFixed(2).toString() + ' - ' + weightCategory(bmi))
